f67cfdcdfe1c505a1ee20279ff457b6f5bbc2427
Nemec Lukas <lukas.nemec2@firma.seznam.cz>
2016-05-25 18:15:51 +0200

    Upgraded digitize and broken stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+extern crate num;
 extern crate num_digitize;
 
+use num::*;
 use num_digitize::digitize;
 
 struct CreditCard {
@@ -17,21 +19,6 @@ impl Validate for CreditCard {
     }
 }
 
-/// Converts a card_number to a vector containing all the digits.
-/// ```
-/// let num = 123;
-/// println!("{:?}", convert_to_digits(num));
-/// [1,2,3]
-/// ```
-fn convert_to_digits(card_number: u64) -> Vec<u8> {
-    let mut digits: Vec<u8> = Vec::new();
-    let digits_long = digitize(card_number);
-    for digit in digits_long.iter() {
-        digits.push(*digit as u8);
-    }
-    return digits;
-}
-
 /// Given a vector containing individual digits of credit card number, return
 /// a vector containing only digits on even indexes.
 /// ```
@@ -40,7 +27,7 @@ fn convert_to_digits(card_number: u64) -> Vec<u8> {
 /// [2,4]
 /// ```
 fn even_digits(card_number_digits: &Vec<u8>) -> Vec<u8> {
-    let even_iter = card_number_digits.iter().enumerate().filter(|&x| x.0 % 2 != 0);
+    let even_iter = card_number_digits.iter().enumerate().filter(|&x| x.0.is_odd());
     let mut digits: Vec<u8> = Vec::new();
     for x in even_iter {
         digits.push(*x.1);
@@ -54,7 +41,7 @@ fn two_times_each_and_sum(digits: &Vec<u8>) -> u64 {
     for num in digits {
         let mut tmp_num: u64 = *num as u64;
         tmp_num *= 2;
-        let multiplication_digits = convert_to_digits(tmp_num);
+        let multiplication_digits = digitize(tmp_num);
         for multiplicated_digit in multiplication_digits {
             result += multiplicated_digit as u64;
         }
@@ -64,7 +51,7 @@ fn two_times_each_and_sum(digits: &Vec<u8>) -> u64 {
 
 fn sum_odd_digits(card_number_digits: &Vec<u8>) -> u64 {
     let mut result: u64 = 0;
-    let num = card_number_digits.iter().enumerate().filter(|&x| x.0 % 2 == 0);
+    let num = card_number_digits.iter().enumerate().filter(|&x| x.0.is_odd());
     for x in num {
         let x = *x.1 as u64;
         result += x;
@@ -73,25 +60,13 @@ fn sum_odd_digits(card_number_digits: &Vec<u8>) -> u64 {
 }
 
 pub fn validate_card_number(card_number: u64) -> bool {
-    let vec = convert_to_digits(card_number);
+    let vec = digitize(card_number);
 
     let odd_sum = sum_odd_digits(&vec);
     let even_sum = two_times_each_and_sum(&even_digits(&vec));
 
     let luhn = odd_sum + even_sum;
     return luhn % 10 == 0;
-}
-
-#[test]
-fn test_convert_to_digits() {
-    let result: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    assert!(convert_to_digits(1234567890) == result);
-}
-
-#[test]
-fn test_convert_to_digits_one() {
-    let result: Vec<u8> = vec![1];
-    assert!(convert_to_digits(1) == result);
 }
 
 #[test]
